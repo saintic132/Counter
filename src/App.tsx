dc9fb941ef4a73f5329af2fb6426c36d68dfb535
saintic <saintic132@gmail.com>
2022-03-27 15:25:33 +0300

    Сделан счетчик 1 месяца

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import s from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [countNumber, setCountNumber] = useState<number>(0);
+    let maxCountNumber = 5
+
+
+    const increaseCountNumber = () => {
+        if (countNumber < maxCountNumber) {
+            setCountNumber(countNumber + 1)
+        }
+    }
+
+    const resetCountNumber = () => {
+        setCountNumber(0)
+    }
+
+    return (
+        <div className={s.app}>
+            <div className={s.body}>
+                <div className={s.showTable}>
+                    <p className={countNumber === maxCountNumber ? s.outputNumberRed : s.outputNumberDefault}>
+                        {countNumber}
+                    </p>
+                </div>
+                <div className={s.button}>
+                    <div className={s.button__increase}>
+                        <button
+                            onClick={increaseCountNumber}
+                            disabled={countNumber === maxCountNumber}
+                        >
+                            inc
+                        </button>
+                    </div>
+                    <div className={s.button__reset}>
+                        <button
+                            onClick={resetCountNumber}
+                            disabled={countNumber !== maxCountNumber}
+                        >
+                            reset
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
